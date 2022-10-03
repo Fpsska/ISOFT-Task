fc2581b2eaaccde53999685f3813dae4edb6eac3
Fpsska <fpsska1337@gmail.com>
2022-10-03 14:28:37 +0300

    feat: create Header.tsx component with logic of display current time & create _vars.scss, _functions.scss files with general app variables, func to calc size of el in REM

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// /. imports
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="container"></div>
+    </header>
+  );
+};
+
+export default Header;
