d7ae213f318d74ba9d506c620543c7c505bfa322
Fpsska <fpsska1337@gmail.com>
2022-10-04 14:18:04 +0300

    feat: create service-worker.ts, serviceWorkerRegistration.ts files & register in index.tsx component & update manifest.json cfg

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from './components/App/App';
 
@@ -23,6 +24,11 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
